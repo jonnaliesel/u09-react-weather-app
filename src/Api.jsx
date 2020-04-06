17b62ca423867f14ed700b991ed0646a9c96797c
Andreas Ny <andreas.ny@chasacademy.se>
2020-04-06 15:50:52 +0200

    Added metric to Api call

--- src/Api.jsx
+++ src/Api.jsx
@@ -15,7 +15,7 @@ class Api extends React.Component {
     const apiKey = process.env.REACT_APP_API_KEY;
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${this.state.currentCity}&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${this.state.currentCity}&units=metric&appid=${apiKey}`
     )
       .then((res) => res.json())
       .then((res) => this.setState({ weatherData: res, isLoaded: true }))
@@ -35,7 +35,19 @@ class Api extends React.Component {
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
-    return <div>{weatherData.city.name} {weatherData.city.country}</div>;
+      return (
+        <div>
+          {weatherData.city.name} {weatherData.city.country}
+          <div>List data</div>
+          {weatherData.list.map((dataTimestamp) => (
+            <div key={dataTimestamp.dt}>
+              <p>Time: {dataTimestamp.dt}</p>
+              {/* <p>{Date.prototype.toLocaleString(dataTimestamp.dt)}</p> */}
+              <p>Temp: {dataTimestamp.main.temp}</p>
+            </div>
+          ))}
+        </div>
+      );
     }
   }
 }
