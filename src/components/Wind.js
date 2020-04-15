29f5eaa7c696e8c0b9e8252e5a59abb1b3a88ea7
Simon Nordström <simon.nordstrom@chasacademy.se>
2020-04-15 15:36:46 +0200

    add arrow in WindComponent with current wind direction

--- src/components/Wind.js
+++ src/components/Wind.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 function Wind(props) {
     return (
         <div>
             <h3>Wind</h3>
-            <p>Speed: {`${props.wind.speed} ${props.speedUnit === 'metric' ? 'km/h' : 'mPh'} NW`}</p>
+            <p>Speed: {`${props.wind.speed} ${props.speedUnit === 'metric' ? 'km/h' : 'mPh'} NW`} <FontAwesomeIcon icon={faArrowUp} transform={{ rotate: props.wind.deg }} /></p>
+            
         </div>
     )
 }
