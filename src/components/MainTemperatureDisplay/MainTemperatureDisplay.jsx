60f251d489842438f8056658b5ce052ddd7dc3aa
Jonna <jonnaliesel@gmail.com>
2020-04-16 12:12:28 +0200

    Create SetTempUnit component

--- src/components/MainTemperatureDisplay/MainTemperatureDisplay.jsx
+++ src/components/MainTemperatureDisplay/MainTemperatureDisplay.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 import Temperature from "../Temperature/Temperature";
+import SetTempUnit from '../SetTempUnit/SetTempUnit';
+
 import styles from './styles.module.css';
 
 
 function MainTemperatureDisplay (props){
+    const { temp, tempUnit, icon } = props;
+
     return (
         <div className="mainTemperatureDisplay">
             <h1 className={styles.h1}>{props.city}</h1>
             <h2 className={styles.h2}><Temperature temp={props.temp} tempUnit={props.tempUnit} /></h2>
+            <SetTempUnit tempUnit={tempUnit}/>
             <img
                 src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}
                 alt=""
---
+++ src/components/SetTempUnit/SetTempUnit.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SetTempUnit(props) {
+    return (
+        <div>
+            <button>{'\u00b0C'}</button>
+            <button>{'\u00b0F'}</button>
+        </div>
+    )
+}
+
+export default SetTempUnit
