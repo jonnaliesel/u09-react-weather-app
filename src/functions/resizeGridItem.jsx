c4f9b7d37bd8347d0fc7457955a0bedc439c717a
Jonna <jonnaliesel@gmail.com>
2020-04-16 21:54:08 +0200

    Delete unused files

--- src/functions/resizeGridItem.jsx
+++
@@ -1,8 +0,0 @@
-// export default function rezizeGridItem(item) {
-
-//     grid = ReactDOM.findDOMNode(<App />).getElementsByClassName("grid")[0];
-//     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-//     rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-//     rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-//     item.style.gridRowEnd = "span "+rowSpan;
-// }
